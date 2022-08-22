6a82752220ffdae1810a6db237bbe233a81ef144
Max Barinov <me@maxbarinov.com>
2022-08-22 12:16:09 +0400

    loado sdk

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+	      <script async src="https://cdn.loado.dev/sdk.js" />
       </Head>
 
       <main className={styles.main}>
